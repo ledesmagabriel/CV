6aa44a277fd8cc83f1ab29c9b92c260f00258819
Ledesma Gabriel <ledesmagabrielg@gmail.com>
2023-02-22 22:56:25 -0300

    Primera version CV

--- js/funciones.js
+++ js/funciones.js
@@ -18,10 +18,3 @@ function Informacion_personal(){
     document.getElementById('Experiencia').style.display = 'block';
     document.getElementById('Referencias-laborales').style.display = 'none';
   } 
-
-  function Referencias_laborales(){
-    document.getElementById('Informacion-personal').style.display = 'none';
-    document.getElementById('Educacion').style.display = 'none';
-    document.getElementById('Experiencia').style.display = 'none';
-    document.getElementById('Referencias-laborales').style.display = 'block';
-  } 
